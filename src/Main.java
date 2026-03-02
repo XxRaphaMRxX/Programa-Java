422a03edf5ffa5b3d2af3ae351dd86d769d9ae9d
rafael <rafamaiarezende@gmail.com>
2026-03-01 21:30:08 -0300

    quarto commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Testando commits");
+        System.out.println("Testando commits fodas");
 
 /*
         boolean a, b, c , d;
